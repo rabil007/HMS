94758e8b8496a2c4704d959ce828876cfe48d4d9
gita608 <rabilmohammed007@gmail.com>
2026-04-23 11:47:12 +0400

    refactor: remove legacy application shell, sidebar, and layout components

--- resources/js/components/app-logo.tsx
+++
@@ -1,9 +0,0 @@
-import AppLogoIcon from '@/components/app-logo-icon';
-
-export default function AppLogo() {
-    return (
-        <div className="flex items-center justify-center bg-transparent">
-            <AppLogoIcon className="h-10 w-auto" />
-        </div>
-    );
-}
